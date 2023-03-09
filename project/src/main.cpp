a3408d746d574139d6273c2caa859111bffbadeb
Guilherme <guilhermersalvador@gmail.com>
2023-03-09 22:54:48 +0000

    Added control over adjacent leds

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,5 +1,5 @@
 #include "painlessMesh.h"
-#include "ArduinoJson.h"
+#include "Arduino_JSON.h"
 #include "WiFi.h"
 
 #define   SSID       "starGazer"
@@ -10,28 +10,39 @@
 #define PIR_PIN 35
 #define LED_PIN 33
 
-Scheduler scheduler; 
+struct NodeInfo {
+  int id;
+  int x_pos;
+  int y_pos;
+};
+
+NodeInfo info;
+
 painlessMesh  mesh;
+Scheduler     scheduler;
+JSONVar turnOnMessage;
 
 int lastReading = 4095;
 
 int pinStateCurrent   = LOW;
 int pinStatePrevious  = LOW;
 
-void sendMessage();
+void turnOff();
 
-Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+Task taskTurnOff( TASK_SECOND * 20 , TASK_FOREVER, &turnOff );
 
-void sendMessage() {
-  String msg = "Ligthing: ";
-  msg = analogRead(LIGHT_SENSOR_PIN);
-  mesh.sendBroadcast(msg);
-  taskSendMessage.setInterval( TASK_SECOND );
+void turnOff() {
+  Serial.println("Turn off");
+  digitalWrite(LED_PIN, LOW);
 }
 
 void receivedCallback(uint32_t from, String &msg) {
-  sscanf(msg.c_str(), "Ligthing: %d", &lastReading);
-  
+  JSONVar receivedMessage = JSON.parse(msg);
+  if (receivedMessage["type"].operator int() == 0 && int(receivedMessage["x_pos"]) - info.x_pos < 2 && int(receivedMessage["y_pos"]) - info.y_pos < 2) {
+    Serial.println("Received turn on message");
+    taskTurnOff.setInterval( TASK_SECOND * 20 );
+    digitalWrite(LED_PIN, HIGH);
+  }
   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
 }
 
@@ -43,8 +54,21 @@ void changedConnectionCallback() {
   Serial.printf("Changed connections\n");
 }
 
+void sendTurnOnMessage() {
+  turnOnMessage["id"] = info.id;
+  turnOnMessage["x_pos"] = info.x_pos;
+  turnOnMessage["y_pos"] = info.y_pos;
+  turnOnMessage["type"] = 0;
+  String jsonString = JSON.stringify(turnOnMessage);
+  mesh.sendBroadcast(jsonString);
+}
+
 void setup() {
   Serial.begin(115200);
+
+  info.id = 1;
+  info.x_pos = 0;
+  info.y_pos = 0;
 
   pinMode(PIR_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
@@ -56,8 +80,8 @@ void setup() {
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
 
-  scheduler.addTask(taskSendMessage);
-  taskSendMessage.enable();
+  scheduler.addTask(taskTurnOff);
+  taskTurnOff.enable();
 
   pinMode(LIGHT_SENSOR_PIN, INPUT);
 }
@@ -69,14 +93,16 @@ void loop() {
 
   if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {
     Serial.println("Motion detected!");
-    if (lastReading < 2048)
+    if (lastReading < 2048) {
+      sendTurnOnMessage();
       digitalWrite(LED_PIN, HIGH);
+      taskTurnOff.setInterval( TASK_SECOND * 20 );
+    }
   }
-  else
-  if (pinStatePrevious == HIGH && pinStateCurrent == LOW) {   // pin state change: HIGH -> LOW
+  else{
+  if (pinStatePrevious == HIGH && pinStateCurrent == LOW) {
     Serial.println("Motion stopped!");
-    digitalWrite(LED_PIN, LOW);
-  }
-
+    //digitalWrite(LED_PIN, LOW);
+  }}
   mesh.update();
 }
