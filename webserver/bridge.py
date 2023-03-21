f679245a4b4966d9f42ad9dcbd59fea3e5c2b0e7
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-03-21 15:34:23 +0000

    fuck mocks

--- webserver/bridge.py
+++ webserver/bridge.py
@@ -1,9 +1,8 @@
 import serial
+import json
 from multiprocessing import Process, Queue
-import random
 
 def read_from_cereal(ser: serial.Serial):
-
     while ser.in_waiting:
         line = ser.readline()
         line = line.decode('utf-8').strip()
@@ -13,16 +12,10 @@ def read_from_cereal(ser: serial.Serial):
 def write_to_cereal(ser: serial.Serial, queue: Queue):
     while True:
         message = queue.get()
-        # write to serial
+
 
 def get_bridge(ser: serial.Serial) -> tuple[Process, Process, Queue]:
     queue = Queue()
     write_process = Process(target=write_to_cereal, args=(ser, queue))
     read_process = Process(target=read_from_cereal, args=(ser,))
     return (read_process, write_process, queue)
-
-
-class MockCereal:
-    def __init__(self, path):
-        self.path = path
-        self.in_waiting = bool(random.randint(0, 1))
