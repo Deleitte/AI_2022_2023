1c4a04078334fd56b4f22a1ba98ed521731e347b
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-03-23 19:10:45 +0000

    update default mongo uri

--- webserver/database.py
+++ webserver/database.py
@@ -4,6 +4,6 @@ from pymongo.database import Database
 
 
 def get_database() -> Database:
-    uri = os.environ.get('STARGAZER_MONGO_URL') or 'mongodb://localhost:27017/'
+    uri = os.environ.get('STARGAZER_MONGO_URL') or 'mongodb://star:drive@localhost:27017/'
     client = MongoClient(uri)
     return client.stargazer
