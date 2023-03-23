00073b429706ef563e1ca23dc61a8ff29cbe66bc
Guilherme <guilhermersalvador@gmail.com>
2023-03-23 17:47:10 +0000

    Add database settings

---
+++ webserver/database.py
@@ -0,0 +1,9 @@
+import os
+from pymongo import MongoClient
+from pymongo.database import Database
+
+
+def get_database() -> Database:
+    uri = os.environ.get('STARGAZER_MONGO_URL') or 'mongodb://localhost:27017/'
+    client = MongoClient(uri)
+    return client.stargazer
