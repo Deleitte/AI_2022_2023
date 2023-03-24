c428adf197bf30d0a878ca505bb5e341489aab5a
Guilherme <guilhermersalvador@gmail.com>
2023-03-24 20:37:51 +0000

    Change scanner floating button

--- webapp/src/routes/home.tsx
+++ webapp/src/routes/home.tsx
@@ -4,61 +4,16 @@ import  QrReader from "react-web-qr-reader";
 import { Button, Paper, Slider, Stack, Typography, Fab, Dialog } from "@mui/material";
 import { QrCode } from "@mui/icons-material";
 import { useState } from "preact/hooks";
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
-import { Box } from "@mui/system";
-
-const fetchedData = [
-  {
-    timestamp: 1679502886893,
-    brightness: 100,
-  },
-  {
-    timestamp: 1679502901566,
-    brightness: 0,
-  },
-  {
-    timestamp: 1679502936610,
-    brightness: 25,
-  },
-  {
-    timestamp: 1679502955022,
-    brightness: 50,
-  },
-  {
-    timestamp: 1679503095266,
-    brightness: 100,
-  },
-  {
-    timestamp: 1679503095276,
-    brightness: 75,
-  },
-  {
-    timestamp: 1679503095286,
-    brightness: 25,
-  },
-  {
-    timestamp: 1679503095296,
-    brightness: 50,
-  },
-];
 
 const Home = () => {
   const [brightness, setBrightness] = useState<number>(0);
 
   const [open, setOpen] = useState(false);
-  const [id, setId] = useState("0");
+  const [id, setId] = useState(0);
 
   const handleScan = (scan) => {
     if (scan) {
-      setId(scan.data);
+      setId(parseInt(scan.data));
       setOpen(false);
     }
   };
@@ -76,13 +31,12 @@ const Home = () => {
     await axios.post("http://localhost:8000/off", { id });
   };
 
-  const data = fetchedData.map((d) => ({
-    timestamp: new Date(d.timestamp).toLocaleString(),
-    brightness: d.brightness,
-  }));
-
   return (
     <>
+      <Fab size="large" color="secondary" onClick={() => setOpen(true)} style={{ position: "fixed", bottom: 30, right: 30, zIndex: 1000 }}>
+        <QrCode />
+      </Fab>
+
       <Paper elevation={3}>
         <Stack spacing={4} sx={{ mb: 1 }} padding={6} textAlign="center">
           <Typography variant="h4">
@@ -115,49 +69,16 @@ const Home = () => {
         </Stack>
       </Paper>
 
-      <Fab size="large" color="secondary" onClick={() => setOpen(true)}>
-        <QrCode />
-      </Fab>
-
       <Dialog fullWidth={true} open={open} onClose={() => setOpen(false)}>
-        <Paper elevation={3}>
-        <Typography variant="h4" textAlign="center" marginY={2}>
-            Station Reader
-        </Typography>
-        <QrReader
-          delay={ 500 }
-          onError={handleError}
-          onScan={handleScan}
-        />
-        </Paper>
-      </Dialog>
-
-      <Paper elevation={3}>
-        <Box textAlign="center">
-          <Typography variant="h4" marginY={4}>
-            Brightness History
+          <Typography variant="h4" textAlign="center" marginY={2}>
+              Station Reader
           </Typography>
-          <ResponsiveContainer width="100%" height={300}>
-            <LineChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <XAxis dataKey="timestamp" />
-              <YAxis dataKey="brightness" />
-              <Tooltip />
-              <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-              <Line type="stepAfter" dataKey="brightness" data={data} />
-            </LineChart>
-          </ResponsiveContainer>
-        </Box>
-      </Paper>
+          <QrReader
+            delay={ 500 }
+            onError={handleError}
+            onScan={handleScan}
+          />
+      </Dialog>
     </>
   );
 };
