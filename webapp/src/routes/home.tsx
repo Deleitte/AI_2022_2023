af9577c40b631803ca26f1ccc3c4105423ee9c87
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-03-25 19:34:55 +0000

    fix typo guilherme made

--- webapp/src/routes/home.tsx
+++ webapp/src/routes/home.tsx
@@ -88,7 +88,7 @@ const Home = () => {
           <TableBody>
             {stations.map((station) => (
               <TableRow
-                key={station.id}
+                key={station.node_id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
