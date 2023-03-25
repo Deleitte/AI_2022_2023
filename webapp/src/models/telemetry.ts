abb7f18c903859a998cd5828ba85d60aaf9553e4
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-03-25 21:12:56 +0000

    add more stuff

---
+++ webapp/src/models/telemetry.ts
@@ -0,0 +1,6 @@
+export default interface Telemetry {
+  brightness: number;
+  node_id: number;
+  locked: boolean;
+  timestamp: string;
+}
--- webapp/src/routes/station.tsx
+++ webapp/src/routes/station.tsx
@@ -1,8 +1,24 @@
-import { Button, List, Paper, Slider, Stack, Typography } from "@mui/material";
+import { Edit } from "@mui/icons-material";
+import {
+  Button,
+  IconButton,
+  Paper,
+  Slider,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import { h, Fragment } from "preact";
 import { route } from "preact-router";
 import { useEffect, useState } from "preact/hooks";
+import Telemetry from "src/models/telemetry";
 
 import Station from "../models/station";
 
@@ -14,6 +30,16 @@ interface Props {
 const StationPage = ({ stationId }: Props) => {
   const [brightness, setBrightness] = useState<number>(0);
   const [station, setStation] = useState<Station>();
+  const [telemetry, setTelemetry] = useState<Telemetry[]>();
+  const [editing, setEditing] = useState(false);
+  const [newName, setNewName] = useState("");
+
+  const changeName = async () => {
+    await axios.put("http://localhost:8000/stations/name", {
+      node_id: stationId,
+      name: newName,
+    });
+  };
 
   const override = async () => {
     await axios.post("http://localhost:8000/on", { brightness, id: stationId });
@@ -28,18 +54,43 @@ const StationPage = ({ stationId }: Props) => {
       const { data } = await axios.get(
         `http://localhost:8000/stations/${stationId}`
       );
-      console.log(data);
+
       setStation(data);
+      if (!editing) setNewName(data.name);
+
+      const { data: telemetry } = await axios.get(
+        `http://localhost:8000/stations/${stationId}/telemetry`
+      );
+
+      setTelemetry(telemetry);
     };
 
-    fetchStation().catch(() => route("/"));
-  }, [stationId]);
+    const interval = setInterval(fetchStation, 2000);
+    return () => clearInterval(interval);
+  }, [stationId, editing]);
 
   return (
     <div>
       {station && (
         <>
-          <Typography variant="h2">{station.name}</Typography>
+          <Typography variant="h2">
+            {editing ? (
+              <TextField
+                value={newName}
+                onChange={(event: any) => setNewName(event.target.value)}
+              />
+            ) : (
+              station.name
+            )}
+            <IconButton
+              onClick={() => {
+                if (editing) changeName();
+                setEditing((value) => !value);
+              }}
+            >
+              <Edit />
+            </IconButton>
+          </Typography>
           <Typography variant="h4">Id: {station.node_id}</Typography>
           <Typography variant="h4">
             Last read on {new Date(station.last_read).toLocaleString()}
@@ -49,7 +100,7 @@ const StationPage = ({ stationId }: Props) => {
           </Typography>
 
           <Paper elevation={3}>
-            <Stack spacing={4} sx={{ mb: 1, mt: 6 }} padding={6} textAlign="center">
+            <Stack spacing={4} sx={{ my: 6 }} padding={6} textAlign="center">
               <Typography variant="h4">
                 Brightness Override: {brightness}%
               </Typography>
@@ -79,6 +130,37 @@ const StationPage = ({ stationId }: Props) => {
               </Button>
             </Stack>
           </Paper>
+
+          {telemetry && (
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Time</TableCell>
+                    <TableCell align="right">Brightness Level</TableCell>
+                    <TableCell align="right">Locked</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {telemetry.map((data) => (
+                    <TableRow
+                      key={data.timestamp}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {new Date(data.timestamp).toLocaleString()}
+                      </TableCell>
+
+                      <TableCell align="right">{data.brightness}</TableCell>
+                      <TableCell align="right">
+                        {data.locked ? "Yes" : "No"}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
         </>
       )}
     </div>
